2623c8a67072879947d3be4025ebb72d23a3f73f
Parinexus <Parinexus@gmail.com>
2025-08-28 18:42:52 +0330

    Add compose components and their resources

---
+++ modules/designsystem/src/main/kotlin/com/parinexus/designsystem/component/TextField.kt
@@ -0,0 +1,249 @@
+package com.parinexus.designsystem.component
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.selection.LocalTextSelectionColors
+import androidx.compose.foundation.text.selection.TextSelectionColors
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.semantics.error
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.Density
+
+@Composable
+fun NoteTextField(
+    modifier: Modifier = Modifier,
+    state: TextFieldState,
+    placeholder: String? = null,
+    imeAction: ImeAction = ImeAction.Done,
+    keyboardAction: () -> Unit = {},
+    maxNum: TextFieldLineLimits = TextFieldLineLimits.Default,
+    textStyle: TextStyle = TextStyle.Default,
+    interactionSource: MutableInteractionSource? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+) {
+    MyTextField(
+        modifier = modifier,
+        state = state,
+        placeholder = {
+            if (placeholder != null) {
+                Text(text = placeholder)
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+
+        keyboardOptions = KeyboardOptions.Default.copy(
+            capitalization = KeyboardCapitalization.Sentences,
+            autoCorrectEnabled = true,
+            imeAction = imeAction,
+        ),
+        keyboardActions = KeyboardActions { keyboardAction() },
+
+        lineLimits = maxNum,
+        trailingIcon = trailingIcon,
+        leadingIcon = leadingIcon,
+        textStyle = textStyle,
+        interactionSource = interactionSource ?: remember { MutableInteractionSource() },
+    )
+}
+
+@Composable
+fun NoteTextField(
+    modifier: Modifier = Modifier,
+    text: String,
+    onTextChange: (String) -> Unit,
+    placeholder: String? = null,
+    imeAction: ImeAction = ImeAction.Done,
+    keyboardAction: () -> Unit = {},
+    maxNum: Int = 1,
+    textStyle: TextStyle = TextStyle.Default,
+    interactionSource: MutableInteractionSource? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    supportingText: String? = null,
+) {
+    TextField(
+        modifier = modifier,
+
+        value = text,
+        onValueChange = onTextChange,
+        isError = isError,
+        supportingText = {
+            if (supportingText != null) {
+                Text(text = supportingText)
+            }
+        },
+        placeholder = {
+            if (placeholder != null) {
+                Text(text = placeholder)
+            }
+        },
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            disabledContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            capitalization = KeyboardCapitalization.Sentences,
+            autoCorrectEnabled = true,
+            imeAction = imeAction,
+        ),
+        keyboardActions = KeyboardActions { keyboardAction() },
+        maxLines = maxNum,
+        trailingIcon = trailingIcon,
+        leadingIcon = leadingIcon,
+        textStyle = textStyle,
+        interactionSource = interactionSource ?: remember { MutableInteractionSource() },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
+@Composable
+fun MyTextField(
+    state: TextFieldState,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    prefix: @Composable (() -> Unit)? = null,
+    suffix: @Composable (() -> Unit)? = null,
+    supportingText: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions.Default,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape = TextFieldDefaults.shape,
+    colors: TextFieldColors = TextFieldDefaults.colors(),
+
+    inputTransformation: InputTransformation? = null,
+    lineLimits: TextFieldLineLimits = TextFieldLineLimits.Default,
+    onTextLayout: (Density.(getResult: () -> TextLayoutResult?) -> Unit)? = null,
+    scrollState: ScrollState = rememberScrollState(),
+
+) {
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor(enabled, isError, interactionSource).value
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    CompositionLocalProvider(LocalTextSelectionColors provides colors.selectionColors) {
+        BasicTextField(
+            state = state,
+            modifier = modifier
+                .defaultErrorSemantics(isError, "Error occur")
+                .defaultMinSize(
+                    minWidth = TextFieldDefaults.MinWidth,
+                    minHeight = TextFieldDefaults.MinHeight,
+                ),
+            enabled = enabled,
+            readOnly = readOnly,
+            textStyle = mergedTextStyle,
+            cursorBrush = SolidColor(colors.cursorColor(isError).value),
+            keyboardOptions = keyboardOptions,
+            interactionSource = interactionSource,
+
+            inputTransformation = inputTransformation,
+            lineLimits = lineLimits,
+            onTextLayout = onTextLayout,
+            decorator = @Composable { innerTextField ->
+                TextFieldDefaults.DecorationBox(
+                    value = state.text.toString(),
+                    visualTransformation = visualTransformation,
+                    innerTextField = innerTextField,
+                    placeholder = placeholder,
+                    label = label,
+                    leadingIcon = leadingIcon,
+                    trailingIcon = trailingIcon,
+                    prefix = prefix,
+                    suffix = suffix,
+                    supportingText = supportingText,
+                    shape = shape,
+                    singleLine = lineLimits == TextFieldLineLimits.SingleLine,
+                    enabled = enabled,
+                    isError = isError,
+                    interactionSource = interactionSource,
+                    colors = colors,
+                )
+            },
+
+            scrollState = scrollState,
+
+        )
+    }
+}
+
+internal val TextFieldColors.selectionColors: TextSelectionColors
+    @Composable get() = textSelectionColors
+
+internal fun Modifier.defaultErrorSemantics(
+    isError: Boolean,
+    defaultErrorMessage: String,
+): Modifier = if (isError) semantics { error(defaultErrorMessage) } else this
+
+@Composable
+internal fun TextFieldColors.cursorColor(isError: Boolean): State<Color> {
+    return rememberUpdatedState(if (isError) errorCursorColor else cursorColor)
+}
+
+@Composable
+internal fun TextFieldColors.textColor(
+    enabled: Boolean,
+    isError: Boolean,
+    interactionSource: InteractionSource,
+): State<Color> {
+    val focused by interactionSource.collectIsFocusedAsState()
+
+    val targetValue = when {
+        !enabled -> disabledTextColor
+        isError -> errorTextColor
+        focused -> focusedTextColor
+        else -> unfocusedTextColor
+    }
+    return rememberUpdatedState(targetValue)
+}
---
+++ modules/designsystem/src/main/kotlin/com/parinexus/designsystem/theme/Theme.kt
@@ -0,0 +1,233 @@
+package com.parinexus.designsystem.theme
+
+import android.os.Build
+import androidx.annotation.ChecksSdkIntAtLeast
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+
+var extendedColorScheme: ExtendedColorScheme = extendedLight
+
+@Composable
+fun SkTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    androidTheme: Boolean = false,
+    disableDynamicTheming: Boolean = true,
+    content: @Composable () -> Unit,
+) {
+    extendedColorScheme = if (darkTheme) extendedDark else extendedLight
+    val colorScheme = when {
+        androidTheme -> if (darkTheme) darkPinkScheme else lightPinkScheme
+        !disableDynamicTheming && supportsDynamicTheming() -> {
+            getDynamicColor(darkTheme)
+        }
+
+        else -> if (darkTheme) darkDefaultScheme else lightDefaultScheme
+    }
+
+    val emptyGradientColors = GradientColors(container = colorScheme.surfaceColorAtElevation(2.dp))
+    val defaultGradientColors = GradientColors(
+        top = colorScheme.inverseOnSurface,
+        bottom = colorScheme.primaryContainer,
+        container = colorScheme.surface,
+    )
+    val gradientColors = when {
+        !disableDynamicTheming && supportsDynamicTheming() -> emptyGradientColors
+        else -> defaultGradientColors
+    }
+    val defaultBackgroundTheme = BackgroundTheme(
+        color = colorScheme.surface,
+        tonalElevation = 2.dp,
+    )
+
+    val tintTheme = when {
+        androidTheme -> TintTheme()
+        !disableDynamicTheming && supportsDynamicTheming() -> TintTheme(colorScheme.primary)
+        else -> TintTheme()
+    }
+    CompositionLocalProvider(
+        LocalGradientColors provides gradientColors,
+        LocalBackgroundTheme provides defaultBackgroundTheme,
+        LocalTintTheme provides tintTheme,
+    ) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = SkTypography,
+            content = content,
+        )
+    }
+}
+
+@ChecksSdkIntAtLeast(api = Build.VERSION_CODES.S)
+fun supportsDynamicTheming() = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+@RequiresApi(Build.VERSION_CODES.S)
+@Composable
+fun getDynamicColor(darkTheme: Boolean): ColorScheme {
+    val context = LocalContext.current
+    return if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+}
+
+val pinkColor = MyColors.Pink
+val lightPinkScheme = lightColorScheme(
+    primary = pinkColor.primaryLight,
+    onPrimary = pinkColor.onPrimaryLight,
+    primaryContainer = pinkColor.primaryContainerLight,
+    onPrimaryContainer = pinkColor.onPrimaryContainerLight,
+    secondary = pinkColor.secondaryLight,
+    onSecondary = pinkColor.onSecondaryLight,
+    secondaryContainer = pinkColor.secondaryContainerLight,
+    onSecondaryContainer = pinkColor.onSecondaryContainerLight,
+    tertiary = pinkColor.tertiaryLight,
+    onTertiary = pinkColor.onTertiaryLight,
+    tertiaryContainer = pinkColor.tertiaryContainerLight,
+    onTertiaryContainer = pinkColor.onTertiaryContainerLight,
+    error = pinkColor.errorLight,
+    onError = pinkColor.onErrorLight,
+    errorContainer = pinkColor.errorContainerLight,
+    onErrorContainer = pinkColor.onErrorContainerLight,
+    background = pinkColor.backgroundLight,
+    onBackground = pinkColor.onBackgroundLight,
+    surface = pinkColor.surfaceLight,
+    onSurface = pinkColor.onSurfaceLight,
+    surfaceVariant = pinkColor.surfaceVariantLight,
+    onSurfaceVariant = pinkColor.onSurfaceVariantLight,
+    outline = pinkColor.outlineLight,
+    outlineVariant = pinkColor.outlineVariantLight,
+    scrim = pinkColor.scrimLight,
+    inverseSurface = pinkColor.inverseSurfaceLight,
+    inverseOnSurface = pinkColor.inverseOnSurfaceLight,
+    inversePrimary = pinkColor.inversePrimaryLight,
+    surfaceDim = pinkColor.surfaceDimLight,
+    surfaceBright = pinkColor.surfaceBrightLight,
+    surfaceContainerLowest = pinkColor.surfaceContainerLowestLight,
+    surfaceContainerLow = pinkColor.surfaceContainerLowLight,
+    surfaceContainer = pinkColor.surfaceContainerLight,
+    surfaceContainerHigh = pinkColor.surfaceContainerHighLight,
+    surfaceContainerHighest = pinkColor.surfaceContainerHighestLight,
+)
+
+val darkPinkScheme = darkColorScheme(
+    primary = pinkColor.primaryDark,
+    onPrimary = pinkColor.onPrimaryDark,
+    primaryContainer = pinkColor.primaryContainerDark,
+    onPrimaryContainer = pinkColor.onPrimaryContainerDark,
+    secondary = pinkColor.secondaryDark,
+    onSecondary = pinkColor.onSecondaryDark,
+    secondaryContainer = pinkColor.secondaryContainerDark,
+    onSecondaryContainer = pinkColor.onSecondaryContainerDark,
+    tertiary = pinkColor.tertiaryDark,
+    onTertiary = pinkColor.onTertiaryDark,
+    tertiaryContainer = pinkColor.tertiaryContainerDark,
+    onTertiaryContainer = pinkColor.onTertiaryContainerDark,
+    error = pinkColor.errorDark,
+    onError = pinkColor.onErrorDark,
+    errorContainer = pinkColor.errorContainerDark,
+    onErrorContainer = pinkColor.onErrorContainerDark,
+    background = pinkColor.backgroundDark,
+    onBackground = pinkColor.onBackgroundDark,
+    surface = pinkColor.surfaceDark,
+    onSurface = pinkColor.onSurfaceDark,
+    surfaceVariant = pinkColor.surfaceVariantDark,
+    onSurfaceVariant = pinkColor.onSurfaceVariantDark,
+    outline = pinkColor.outlineDark,
+    outlineVariant = pinkColor.outlineVariantDark,
+    scrim = pinkColor.scrimDark,
+    inverseSurface = pinkColor.inverseSurfaceDark,
+    inverseOnSurface = pinkColor.inverseOnSurfaceDark,
+    inversePrimary = pinkColor.inversePrimaryDark,
+    surfaceDim = pinkColor.surfaceDimDark,
+    surfaceBright = pinkColor.surfaceBrightDark,
+    surfaceContainerLowest = pinkColor.surfaceContainerLowestDark,
+    surfaceContainerLow = pinkColor.surfaceContainerLowDark,
+    surfaceContainer = pinkColor.surfaceContainerDark,
+    surfaceContainerHigh = pinkColor.surfaceContainerHighDark,
+    surfaceContainerHighest = pinkColor.surfaceContainerHighestDark,
+)
+
+val defaultColor = MyColors.Default
+val lightDefaultScheme = lightColorScheme(
+    primary = defaultColor.primaryLight,
+    onPrimary = defaultColor.onPrimaryLight,
+    primaryContainer = defaultColor.primaryContainerLight,
+    onPrimaryContainer = defaultColor.onPrimaryContainerLight,
+    secondary = defaultColor.secondaryLight,
+    onSecondary = defaultColor.onSecondaryLight,
+    secondaryContainer = defaultColor.secondaryContainerLight,
+    onSecondaryContainer = defaultColor.onSecondaryContainerLight,
+    tertiary = defaultColor.tertiaryLight,
+    onTertiary = defaultColor.onTertiaryLight,
+    tertiaryContainer = defaultColor.tertiaryContainerLight,
+    onTertiaryContainer = defaultColor.onTertiaryContainerLight,
+    error = defaultColor.errorLight,
+    onError = defaultColor.onErrorLight,
+    errorContainer = defaultColor.errorContainerLight,
+    onErrorContainer = defaultColor.onErrorContainerLight,
+    background = defaultColor.backgroundLight,
+    onBackground = defaultColor.onBackgroundLight,
+    surface = defaultColor.surfaceLight,
+    onSurface = defaultColor.onSurfaceLight,
+    surfaceVariant = defaultColor.surfaceVariantLight,
+    onSurfaceVariant = defaultColor.onSurfaceVariantLight,
+    outline = defaultColor.outlineLight,
+    outlineVariant = defaultColor.outlineVariantLight,
+    scrim = defaultColor.scrimLight,
+    inverseSurface = defaultColor.inverseSurfaceLight,
+    inverseOnSurface = defaultColor.inverseOnSurfaceLight,
+    inversePrimary = defaultColor.inversePrimaryLight,
+    surfaceDim = defaultColor.surfaceDimLight,
+    surfaceBright = defaultColor.surfaceBrightLight,
+    surfaceContainerLowest = defaultColor.surfaceContainerLowestLight,
+    surfaceContainerLow = defaultColor.surfaceContainerLowLight,
+    surfaceContainer = defaultColor.surfaceContainerLight,
+    surfaceContainerHigh = defaultColor.surfaceContainerHighLight,
+    surfaceContainerHighest = defaultColor.surfaceContainerHighestLight,
+)
+
+val darkDefaultScheme = darkColorScheme(
+    primary = defaultColor.primaryDark,
+    onPrimary = defaultColor.onPrimaryDark,
+    primaryContainer = defaultColor.primaryContainerDark,
+    onPrimaryContainer = defaultColor.onPrimaryContainerDark,
+    secondary = defaultColor.secondaryDark,
+    onSecondary = defaultColor.onSecondaryDark,
+    secondaryContainer = defaultColor.secondaryContainerDark,
+    onSecondaryContainer = defaultColor.onSecondaryContainerDark,
+    tertiary = defaultColor.tertiaryDark,
+    onTertiary = defaultColor.onTertiaryDark,
+    tertiaryContainer = defaultColor.tertiaryContainerDark,
+    onTertiaryContainer = defaultColor.onTertiaryContainerDark,
+    error = defaultColor.errorDark,
+    onError = defaultColor.onErrorDark,
+    errorContainer = defaultColor.errorContainerDark,
+    onErrorContainer = defaultColor.onErrorContainerDark,
+    background = defaultColor.backgroundDark,
+    onBackground = defaultColor.onBackgroundDark,
+    surface = defaultColor.surfaceDark,
+    onSurface = defaultColor.onSurfaceDark,
+    surfaceVariant = defaultColor.surfaceVariantDark,
+    onSurfaceVariant = defaultColor.onSurfaceVariantDark,
+    outline = defaultColor.outlineDark,
+    outlineVariant = defaultColor.outlineVariantDark,
+    scrim = defaultColor.scrimDark,
+    inverseSurface = defaultColor.inverseSurfaceDark,
+    inverseOnSurface = defaultColor.inverseOnSurfaceDark,
+    inversePrimary = defaultColor.inversePrimaryDark,
+    surfaceDim = defaultColor.surfaceDimDark,
+    surfaceBright = defaultColor.surfaceBrightDark,
+    surfaceContainerLowest = defaultColor.surfaceContainerLowestDark,
+    surfaceContainerLow = defaultColor.surfaceContainerLowDark,
+    surfaceContainer = defaultColor.surfaceContainerDark,
+    surfaceContainerHigh = defaultColor.surfaceContainerHighDark,
+    surfaceContainerHighest = defaultColor.surfaceContainerHighestDark,
+)
