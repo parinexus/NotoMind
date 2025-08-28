8853535c1bdecd49a27c6e372966e97439ff9f28
Parinexus <Parinexus@gmail.com>
2025-08-28 17:38:06 +0330

    Add gradle and gitignore files

---
+++ modules/data/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("parinexus.android.library")
+    id("parinexus.android.hilt")
+    id("kotlinx-serialization")
+}
+
+android {
+    namespace = "com.parinexus.data"
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+            isReturnDefaultValues = true
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.modules.database)
+    implementation(projects.modules.datastore)
+    api(projects.modules.domain)
+    api(projects.modules.common)
+    api(projects.modules.model)
+    implementation(projects.modules.analytics)
+
+    implementation(libs.kotlinx.datetime)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.kotlinx.serialization.json)
+    testImplementation(projects.modules.testing)
+
+    testImplementation(libs.junit4)
+    testImplementation(libs.truth)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlin.test.junit5)
+
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.core)
+    androidTestImplementation(libs.androidx.test.rules)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+    androidTestImplementation(libs.androidx.compose.ui.test)
+    debugImplementation(libs.androidx.compose.ui.testManifest)
+}
